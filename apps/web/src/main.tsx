a4c88b8034671f6b4be6924860609653fe322ed6
kurniawans putra <kurniawansputra02@gmail.com>
2026-08-31 05:02:46 +0700

    style(web): use Roboto as default font

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
-import '@fontsource-variable/plus-jakarta-sans';
+import '@fontsource-variable/roboto';
 import App from './App';
 import './styles-clean.css';
 
